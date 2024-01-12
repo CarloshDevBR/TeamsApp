2137b295eb977c00687b988e3a929a859a8c8a62
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-12 11:55:07 -0300

    feat: pagina Groups criada

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Groups() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-600">
+      <Text className="text-red-800">Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+}
