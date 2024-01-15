24800b2f31410e7306a1e56134037b79a8bc1e0a
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-15 17:00:00 -0300

    chore: cor configurada

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ const colors = {
   'black-secondary': '#00B37E',
   'black-terciary': '#29292E',
   'black-fourth': '#323238',
+  'black-five': '#202024',
   'gray-primary': '#7C7C8A',
   'gray-secondary': '#C4C4CC',
   'gray-terciary': '#E1E1E6',
