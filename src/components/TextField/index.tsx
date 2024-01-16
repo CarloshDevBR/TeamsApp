0dd6ccf8f3022303feb396260b1c5f21e1e69a97
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:03:29 -0300

    feat: prop containerStyles passada

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,9 +1,18 @@
-import { TextInput, TextInputProps } from 'react-native';
+import { TextInput, TextInputProps, View } from 'react-native';
+
+import { ButtonIcon } from '../ButtonIcon';
 
 type TextFieldProps = TextInputProps & {
+  containerStyles: string;
   iconEnd?: React.ReactNode;
 };
 
-export const TextField = ({ iconEnd, ...rest }: TextFieldProps) => {
-  return <TextInput className="h-[56px] pl-[21px] bg-black-primary rounded-md text-white font-normal" placeholderTextColor="#7C7C8A" {...rest} />;
+export const TextField = ({ containerStyles, iconEnd, ...rest }: TextFieldProps) => {
+  return (
+    <View className={`w-full h-[56px] flex flex-row items-center pl-[21px] pr-3 bg-black-primary rounded-md ${containerStyles}`}>
+      <TextInput className="h-full flex-1 text-white" placeholderTextColor="#7C7C8A" autoCorrect={false} {...rest} />
+
+      <ButtonIcon icon={iconEnd} />
+    </View>
+  );
 };
