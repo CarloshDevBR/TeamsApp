199c58de90b5ba69312dc089e496b73b47221a84
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:02:21 -0300

    feat: navegacao adicionada

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import { Image, TouchableOpacity, View } from 'react-native';
 
 import { CaretLeft } from 'phosphor-react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
 import logo from '@assets/logo.png';
 
 type HeaderProps = {
@@ -9,10 +11,12 @@ type HeaderProps = {
 };
 
 export const Header = ({ showBackButton }: HeaderProps) => {
+  const navigation = useNavigation();
+
   return (
     <View className={`w-full flex-row items-center ${showBackButton ? 'justify-between' : 'justify-center'} py-[12px]`}>
       {showBackButton && (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('groups')}>
           <CaretLeft color="#FFF" size={36} />
         </TouchableOpacity>
       )}
