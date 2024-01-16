cd3251dee8b0ebebcfe36d784859e40407cf3ef2
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:03:48 -0300

    feat: listagem dos grupos do localStorage criado

--- src/screens/Groups/components/ListGroups/index.tsx
+++ src/screens/Groups/components/ListGroups/index.tsx
@@ -1,21 +1,37 @@
+import { useCallback, useEffect, useState } from 'react';
+
 import { FlatList } from 'react-native';
 
 import { GroupCard } from '../GroupCard';
 
-import { ListEmpty } from '@components/index';
+import { ListEmpty } from '@components/ListEmpty';
+
+import { GroupGetAll } from 'src/Storage/group/GroupGetAll';
+
+import { useFocusEffect } from '@react-navigation/native';
 
 import { Group } from 'src/types';
 
-type ListGroupsProps = {
-  data?: Group[];
-};
+export const ListGroups = () => {
+  const [groups, setGrups] = useState<Group[]>();
 
-export const ListGroups = ({ data = [] }: ListGroupsProps) => {
+  const fetchGroups = useCallback(async () => {
+    const data = await GroupGetAll();
+
+    setGrups(data);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
+
   return (
     <FlatList
       keyExtractor={(item) => item.id}
-      data={data}
-      contentContainerStyle={data?.length === 0 && { flex: 1 }}
+      data={groups}
+      contentContainerStyle={groups?.length === 0 && { flex: 1 }}
       renderItem={({ item }) => <GroupCard title={item.title} />}
       ListEmptyComponent={<ListEmpty title="Crie uma nova turma" />}
     />
