264f2344244ced67ed52427b77e1531d8089db58
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:00:43 -0300

    fix: arquivo index removido por problemas de import

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import { GlobalLoading } from './GlobalLoading';
-import { Header } from './Header';
-import { HighLight } from './HighLight';
-import { Button } from './Button';
-import { ListEmpty } from './ListEmpty';
-import { TextField } from './TextField';
-
-export { GlobalLoading, Header, HighLight, Button, ListEmpty, TextField };
