2c316c168abe325bbfe9e8cdd4026b902e37fbbe
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:04:45 -0300

    chore: tipagem Filter e Player criada

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,14 @@ export interface Group {
   id: string;
   title: string;
 }
+
+export interface Filter {
+  id: string;
+  title: string;
+  active: boolean;
+}
+
+export interface Player {
+  id: string;
+  name: string;
+}
