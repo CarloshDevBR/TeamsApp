d05fe0c11c8fd6e0e53abc2178f4750a9bbc602d
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-16 17:04:12 -0300

    feat: componentes ListPlayers e Player criados

---
+++ src/screens/Players/components/ListPlayers/index.tsx
@@ -0,0 +1,23 @@
+import { FlatList } from 'react-native';
+
+import { Player } from './player';
+
+import { Player as PPRops } from 'src/types';
+
+import { ListEmpty } from '@components/ListEmpty';
+
+type ListPlayersProps = {
+  data: PPRops[];
+};
+
+export const ListPlayers = ({ data = [] }: ListPlayersProps) => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <Player data={item} />}
+      ListEmptyComponent={<ListEmpty title="Não há pessoas nesse time." />}
+      contentContainerStyle={data?.length === 0 && { flex: 1 }}
+    />
+  );
+};
---
+++ src/screens/Players/components/ListPlayers/player.tsx
@@ -0,0 +1,23 @@
+import { Text, View } from 'react-native';
+
+import { User, X } from 'phosphor-react-native';
+
+import { Player as PPRops } from 'src/types';
+
+import { ButtonIcon } from '@components/ButtonIcon';
+
+type PlayerProps = {
+  data: PPRops;
+};
+
+export const Player = ({ data }: PlayerProps) => {
+  return (
+    <View className="h-[54px] flex items-center flex-row pl-[15px] pr-[20px] bg-black-terciary rounded mb-[10px]">
+      <User size={24} weight="fill" color="#C4C4CC" />
+
+      <Text className="flex-1 text-gray-secondary pl-[10px]">{data.name}</Text>
+
+      <ButtonIcon icon={<X size={24} color="#CC2937" />} />
+    </View>
+  );
+};
