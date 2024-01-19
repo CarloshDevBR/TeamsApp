3c61995c8cabf7eba2d6eb5a3fc0301d78f6cd52
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-19 16:00:27 -0300

    feat: onPressButton adicionado no component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -5,14 +5,15 @@ import { ButtonIcon } from '../ButtonIcon';
 type TextFieldProps = TextInputProps & {
   containerStyles: string;
   iconEnd?: React.ReactNode;
+  onPressButton?: () => void;
 };
 
-export const TextField = ({ containerStyles, iconEnd, ...rest }: TextFieldProps) => {
+export const TextField = ({ containerStyles, iconEnd, onPressButton, ...rest }: TextFieldProps) => {
   return (
     <View className={`w-full h-[56px] flex flex-row items-center pl-[21px] pr-3 bg-black-primary rounded-md ${containerStyles}`}>
       <TextInput className="h-full flex-1 text-white" placeholderTextColor="#7C7C8A" autoCorrect={false} {...rest} />
 
-      <ButtonIcon icon={iconEnd} />
+      <ButtonIcon icon={iconEnd} onPress={onPressButton} />
     </View>
   );
 };
