601aa5e37dfe539e5590d4a6f473825ceb7a4836
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-19 16:10:31 -0300

    feat: navegacao criada e funcao getStorage refatorada

--- src/screens/Groups/components/GroupCard/index.tsx
+++ src/screens/Groups/components/GroupCard/index.tsx
@@ -1,18 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
+
 import { UsersThree } from 'phosphor-react-native';
 
 import { Text, TouchableOpacity, View } from 'react-native';
 
+import { Group } from 'src/types';
+
 type GroupCardProps = {
-  title: string;
+  data: Group;
 };
 
-export const GroupCard = ({ title }: GroupCardProps) => {
+export const GroupCard = ({ data }: GroupCardProps) => {
+  const navigation = useNavigation();
+
+  const handlePlayers = () => {
+    navigation.navigate('players', { id: data.id });
+  };
+
   return (
-    <TouchableOpacity className="h-[96px] bg-black-terciary rounded-md mb-[10px]">
+    <TouchableOpacity className="h-[96px] bg-black-terciary rounded-md mb-[10px]" onPress={handlePlayers}>
       <View className="flex-1 flex flex-row items-center ml-[29px]">
         <UsersThree color="#00B37E" size={32} weight="fill" />
 
-        <Text className="text-gray-terciary ml-[20px]">{title}</Text>
+        <Text className="text-gray-terciary ml-[20px]">{data.title}</Text>
       </View>
     </TouchableOpacity>
   );
--- src/screens/Groups/components/ListGroups/index.tsx
+++ src/screens/Groups/components/ListGroups/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { FlatList } from 'react-native';
 
@@ -6,17 +6,19 @@ import { GroupCard } from '../GroupCard';
 
 import { ListEmpty } from '@components/ListEmpty';
 
-import { GroupGetAll } from 'src/Storage/group/GroupGetAll';
+import { getStorage } from '@storage/getStorage';
 
 import { useFocusEffect } from '@react-navigation/native';
 
 import { Group } from 'src/types';
 
+import { KEY_GROUP } from '@constants/index';
+
 export const ListGroups = () => {
   const [groups, setGrups] = useState<Group[]>();
 
   const fetchGroups = useCallback(async () => {
-    const data = await GroupGetAll();
+    const data: Group[] = await getStorage(KEY_GROUP);
 
     setGrups(data);
   }, []);
@@ -29,10 +31,10 @@ export const ListGroups = () => {
 
   return (
     <FlatList
-      keyExtractor={(item) => item.id}
       data={groups}
-      contentContainerStyle={groups?.length === 0 && { flex: 1 }}
-      renderItem={({ item }) => <GroupCard title={item.title} />}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={{ flex: 1 }}
+      renderItem={({ item }) => <GroupCard data={item} />}
       ListEmptyComponent={<ListEmpty title="Crie uma nova turma" />}
     />
   );
