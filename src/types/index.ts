f3fd9cd83192e766812fd2e369b0008acecbe34e
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-19 16:14:00 -0300

    chore: tipagem criada e alterada

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,10 +6,15 @@ export interface Group {
 export interface Filter {
   id: string;
   title: string;
-  active: boolean;
 }
 
 export interface Player {
   id: string;
   name: string;
+  time: Time;
+}
+
+export interface Time {
+  id: string;
+  title: string;
 }
