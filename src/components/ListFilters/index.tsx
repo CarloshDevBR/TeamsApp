fccfa0ddaa128b6d723efb92a6a88fb6ed306ec0
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-19 16:09:52 -0300

    feat: onSelectFilter adicionado

--- src/components/ListFilters/index.tsx
+++ src/components/ListFilters/index.tsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
+
 import { FlatList } from 'react-native';
 
 import { Filter as Ft } from './filter';
 
-import { Filter as FtProps } from 'src/types';
+import { Filter as FtProps, Time } from 'src/types';
 
 type ListFiltersProps = {
   data: FtProps[];
-  containerStyles?: string;
-  onPress?: (data: FtProps) => void;
+  onSelectFilter: (item: Time) => void;
 };
 
-export const ListFilters = ({ data = [], onPress, containerStyles }: ListFiltersProps) => {
+export const ListFilters = ({ data = [], onSelectFilter }: ListFiltersProps) => {
+  const [selected, setSelected] = useState<string>(data[0].id || '');
+
   return (
     <FlatList
       className="mr-2"
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => <Ft data={item} isActive={item.active} onPress={onPress} />}
+      renderItem={({ item }) => (
+        <Ft
+          data={item}
+          isActive={item.id === selected}
+          onPress={(item) => {
+            setSelected(item.id);
+
+            onSelectFilter(item);
+          }}
+        />
+      )}
       horizontal
     />
   );
