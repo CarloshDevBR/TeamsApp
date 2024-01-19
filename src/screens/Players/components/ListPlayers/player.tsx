12ba666a28eb922cc03bd445e9864f45b32b8e8b
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-19 16:13:39 -0300

    feat: deletar player adicionado

--- src/screens/Players/components/ListPlayers/player.tsx
+++ src/screens/Players/components/ListPlayers/player.tsx
@@ -6,18 +6,22 @@ import { Player as PPRops } from 'src/types';
 
 import { ButtonIcon } from '@components/ButtonIcon';
 
+import { usePlayers } from '../../../../context/players';
+
 type PlayerProps = {
   data: PPRops;
 };
 
 export const Player = ({ data }: PlayerProps) => {
+  const { deletePlayer } = usePlayers();
+
   return (
     <View className="h-[54px] flex items-center flex-row pl-[15px] pr-[20px] bg-black-terciary rounded mb-[10px]">
       <User size={24} weight="fill" color="#C4C4CC" />
 
       <Text className="flex-1 text-gray-secondary pl-[10px]">{data.name}</Text>
 
-      <ButtonIcon icon={<X size={24} color="#CC2937" />} />
+      <ButtonIcon icon={<X size={24} color="#CC2937" />} onPress={() => deletePlayer(data.id)} />
     </View>
   );
 };
