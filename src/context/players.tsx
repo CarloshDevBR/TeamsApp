838fe5a5ec86733d32ffc874ce1b97edb0c987c7
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-22 10:14:50 -0300

    refactor: loading adicionado e funcao getPlayers alterada

--- src/context/players.tsx
+++ src/context/players.tsx
@@ -1,8 +1,6 @@
-import { useContext, createContext, useCallback, useState } from 'react';
+import { useContext, createContext, useCallback, useState, useEffect } from 'react';
 
 import { KEY_PLAYERS } from '@constants/index';
-
-import { useFocusEffect } from '@react-navigation/native';
 
 import { getStorage } from '@storage/getStorage';
 
@@ -13,32 +11,46 @@ import { deleteItemOnList } from '@functions/deleteItemOnList';
 
 interface PlayersContext {
   players: Player[];
+  getPlayers: (id: string) => void;
   addPlayer: (newPlayer: Player) => Promise<void>;
   deletePlayer: (id: string) => Promise<void>;
+  isLoading: boolean;
 }
 
 const PlayersContext = createContext<PlayersContext>({
   players: [],
+  getPlayers: async () => {},
   addPlayer: async () => {},
   deletePlayer: async () => {},
+  isLoading: false,
 });
 
 export const PlayersProvider = ({ children }: { children: React.ReactNode }) => {
+  const [all, setAll] = useState<Player[]>([]);
   const [players, setPlayers] = useState<Player[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const fetchPlayers = useCallback(async () => {
+  const getPlayers = useCallback(async (id: string) => {
+    setIsLoading(true);
+
     const data: Player[] = await getStorage(KEY_PLAYERS);
 
-    setPlayers(data);
+    const filteredGroup = data.filter((item) => item.groupId === id);
+
+    setPlayers(filteredGroup);
+
+    setAll(data);
+
+    setIsLoading(false);
   }, []);
 
   const addPlayer = async (newPlayer: Player) => {
     if (newPlayer.name) {
-      const newArray: Player[] = [...players, newPlayer];
-
-      setPlayers(newArray);
+      const newArray: Player[] = [...all, newPlayer];
 
       await createStorage({ key: KEY_PLAYERS, value: newArray });
+
+      await getPlayers(newPlayer.groupId);
     }
   };
 
@@ -50,13 +62,7 @@ export const PlayersProvider = ({ children }: { children: React.ReactNode }) => 
     await createStorage({ key: KEY_PLAYERS, value: removedPlayerOnList });
   };
 
-  useFocusEffect(
-    useCallback(() => {
-      fetchPlayers();
-    }, [])
-  );
-
-  return <PlayersContext.Provider value={{ players, addPlayer, deletePlayer }}>{children}</PlayersContext.Provider>;
+  return <PlayersContext.Provider value={{ players, getPlayers, addPlayer, deletePlayer, isLoading }}>{children}</PlayersContext.Provider>;
 };
 
 export const usePlayers = () => {
